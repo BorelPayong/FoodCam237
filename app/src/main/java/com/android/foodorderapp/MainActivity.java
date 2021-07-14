d7d5ab80eb5d81be7830bbf898848721f7580864
BorelPayong <68334292+BorelPayong@users.noreply.github.com>
2021-07-14 16:50:05 +0200

    add projet
    add projet

---
+++ app/src/main/java/com/android/foodorderapp/MainActivity.java
@@ -0,0 +1,106 @@
+package com.android.foodorderapp;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+
+import com.android.foodorderapp.adapters.RestaurantListAdapter;
+import com.android.foodorderapp.model.RestaurantModel;
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements RestaurantListAdapter.RestaurantListClickListener {
+
+    ConstraintLayout bgimage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AnimationDrawable animationDrawable = new AnimationDrawable();
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.bg2),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img2),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img3),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img4),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img5),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img6),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img7),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img8),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.bg3),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img9),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img10),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img11),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img11),3000);
+
+
+        animationDrawable.setOneShot(false);
+        animationDrawable.setEnterFadeDuration(850);
+        animationDrawable.setExitFadeDuration(1600);
+
+
+        bgimage = findViewById(R.id.back3);
+        bgimage.setBackgroundDrawable(animationDrawable);
+        animationDrawable.start();
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Liste des Restaurants");
+
+        List<RestaurantModel> restaurantModelList =  getRestaurantData();
+
+        initRecyclerView(restaurantModelList);
+    }
+
+    private void initRecyclerView(List<RestaurantModel> restaurantModelList ) {
+        RecyclerView recyclerView =  findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        RestaurantListAdapter adapter = new RestaurantListAdapter(restaurantModelList, this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private List<RestaurantModel> getRestaurantData() {
+        InputStream is = getResources().openRawResource(R.raw.restaurent);
+        Writer writer = new StringWriter();
+        char[] buffer = new char[1024];
+        try{
+            Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            int n;
+            while(( n = reader.read(buffer)) != -1) {
+                writer.write(buffer, 0,n);
+            }
+        }catch (Exception e) {
+
+        }
+
+        String jsonStr = writer.toString();
+        Gson gson = new Gson();
+        RestaurantModel[] restaurantModels =  gson.fromJson(jsonStr, RestaurantModel[].class);
+        List<RestaurantModel> restList = Arrays.asList(restaurantModels);
+
+        return  restList;
+
+    }
+
+    @Override
+    public void onItemClick(RestaurantModel restaurantModel) {
+        Intent intent = new Intent(MainActivity.this, RestaurantMenuActivity.class);
+        intent.putExtra("RestaurantModel", restaurantModel);
+        startActivity(intent);
+
+    }
+}
